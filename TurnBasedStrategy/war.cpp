0e9be34ffe33341f870511665df9709a105bf5b9
Sergey <Sochnevs@list.ru>
2021-01-22 06:15:14 +0300

    add some methods to General class

--- TurnBasedStrategy/war.cpp
+++ TurnBasedStrategy/war.cpp
@@ -2,23 +2,64 @@
 
 void General::Workout()
 {
-	skill_float_ += ((intelegence_ / 10) * (spirit_ / 100));
-	skill_ = static_cast<unsigned>(skill_float_);
+	action_ = 2;
+	//skill_float_ += ((intelegence_ / 10) * (spirit_ / 100));
+	//skill_ = static_cast<unsigned>(skill_float_);
 }
 
 void General::Study()
 {
-	++intelegence_;
+	
+	action_ = 1;
 }
 
-General::General(std::string name, unsigned skill, unsigned spirit, unsigned intelegence, unsigned age): skill_(skill), intelegence_(intelegence), spirit_(spirit), age_(age), name_(name)
+void General::Defense(unsigned count_defenders)
+{
+	action_ = 3;
+	count_solders_ = count_defenders;
+}
+
+void General::NextTurn()
+{
+	switch (action_)
+	{
+	case 0:
+		spirit_ += 5;
+		if (spirit_ > 100)spirit_ = 100;
+	case 1:
+		++intelegence_;
+	case 2:
+		skill_float_ += ((intelegence_ / 10) * (spirit_ / 100));
+		skill_ = static_cast<unsigned>(skill_float_);
+		if (skill_ > 100) skill_ = 100;
+	default:
+		break;
+	}
+}
+
+General::General(std::string name, unsigned skill, unsigned spirit, unsigned speed_, unsigned intelegence, unsigned age): skill_(skill), intelegence_(intelegence), spirit_(spirit), speed_(speed), age_(age), name_(name)
 {
 	skill_float_ = static_cast<float>(skill);	
+}
+
+void General::AttackTo(unsigned count_attack, unsigned number_kingd)
+{
+	count_solders_ = count_attack;
+}
+
+void General::Rest()
+{
+	action_ = 0;
 }
 
 void Kingdoom_defense::AddSolder(unsigned count)
 {
 	solders_+=count;	
+}
+
+unsigned Kingdoom_defense::GetCountSpecialists()
+{
+	return solders_;
 }
 
 void Kingdoom_defense::AddGeneral(std::string name, unsigned skill, unsigned intelegence, unsigned age)
--- TurnBasedStrategy/war.h
+++ TurnBasedStrategy/war.h
@@ -4,7 +4,7 @@
 #ifndef WAR 
 #define WAR
 #include<string>
-#include"Engine.h"
+#include"engine.h"
 #include<vector>
 
 // правила войны
@@ -13,15 +13,18 @@ class General
 {
 	float skill_float_;
 public:
-	unsigned skill; // max 100
+	unsigned skill_; // max 100
 	unsigned intelegence_; // max 100
 	unsigned spirit_; // max 100
+	unsigned speed_; // max 100
 	unsigned age_;   // max 100
 	unsigned action_;  // 0 - rest, 1 - study,2- workout, 3- defense, 4 ... attack to kingdom  TODO: action Generall
 	unsigned target_;  // number of kingdom to attack
 	unsigned count_solders_;
 	std::string name_;
-	General(std::string name,unsigned skill, unsigned intelegence, unsigned spirit, unsigned age);
+	General(std::string name,unsigned skill, unsigned intelegence, unsigned spirit, unsigned speed, unsigned age);
+	void AttackTo(unsigned count_attack, unsigned number_kingd);
+	void Rest();
 	void Workout();
 	void Study();
 	void Defense(unsigned count_defenders);
