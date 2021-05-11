e7586cfbee561b4c9a49e6dc2a11a1bf55ba9a49
Sergey <Sochnevs@list.ru>
2021-05-11 22:05:13 +0300

    set test

--- EngineTest/fake_game_obj.cpp
+++
@@ -1,9 +0,0 @@
-#include"fake_game_obj.h"
-
-fake_game_obj::fake_game_obj(){
-	++counter;
-}
-
-void fake_game_obj::SetInterface(std::vector<EngineGameObjInterface*> list_in){
-	another = list_in[0];
-}
--- EngineTest/test.cpp
+++ EngineTest/test.cpp
@@ -1,5 +1,4 @@
 #include "pch.h"
-#include "fake_game_obj.h"
 #include <vector>
 
 TEST(EngineTests, ExampleTest) {
@@ -8,12 +7,3 @@ TEST(EngineTests, ExampleTest) {
 }
 
 
-TEST(EnginePolimorphTests, SetInterfaceTest){
-	fake_game_obj f1;
-	fake_game_obj f2;
-	std::vector<EngineGameObjInterface*> vEGameObjs;
-	vEGameObjs.push_back(f1);
-	vEGameObjs.push_back(f2);
-	f2.SetInterface(vEGameObjs);
-
-}
