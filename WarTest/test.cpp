070d8f0efee66ed98f57917ad4a67defd687ccf8
Sergey <sochnev@list.ru>
2021-05-14 06:29:40 +0300

    add config

--- WarTest/test.cpp
+++ WarTest/test.cpp
@@ -1,6 +1,16 @@
 #include "pch.h"
+#include "../TurnBasedStrategy/war.h"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+namespace GeneralNameSpace {
+
+	TEST(GeneralClassTest, GeneralConstructor_Floating_to_Int) {
+		Defense d;
+		Kingdoom_defense kd(0,d);
+		kd.AddGeneral("landaun", 10, 10, 10, 10);
+	}
 }
